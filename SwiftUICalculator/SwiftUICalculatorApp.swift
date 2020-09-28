eff35dde65060c755c528a64d79017cccae43c4e
ShoheiSugawa <shohei.sugawa@livesense.co.jp>
2020-09-28 16:07:41 +0900

    Initial Commit

---
+++ SwiftUICalculator/SwiftUICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICalculatorApp.swift
+//  SwiftUICalculator
+//
+//  Created by Shohei Sugawa on 2020/09/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
